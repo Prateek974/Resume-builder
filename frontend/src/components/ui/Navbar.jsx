1d6372f5640fd0ba865dae567d3b5d9988ca26b2
Prateek Sharma <b240754@skit.ac.in>
2026-01-27 10:16:29 +0530

    fixed css error and made navbar

--- frontend/src/components/ui/Navbar.jsx
+++ frontend/src/components/ui/Navbar.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 const Navbar = () => {
     return(
-    <header className="header-contianer">
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <a>SmartResume builder</a>
-            </div>
-
-            <ul className="navbar-links">
-                <li>
-                    <a>Home</a>
-                </li>
-                <li>
-                    <a>contact</a>
-                </li>
-            </ul>
-        </nav>
+        <header className="border-b p-4 bg-white-500 shadow-sm">
+      <nav className="flex justify-between items-center max-w-7xl mx-auto">
+        <div className="text-xl font-bold text-indigo-600">
+          SmartResume AI
+        </div>
+        <ul className="flex space-x-6 text-gray-600 font-medium">
+          <li><a href="/" className="hover:text-indigo-600">Home</a></li>
+          <li><a href="/contact" className="hover:text-indigo-600">Contact</a></li>
+          <li><a href="/pricing" className="hover:text-indigo-600">Blog</a></li>
+          <li><a href="/pricing" className="hover:text-indigo-600">Features</a></li>
+          <li><a href="/pricing" className="hover:text-indigo-600">Pricing</a></li>
+        </ul>
+      </nav>
     </header>
-); 
+
+    );
 };
  export default Navbar;
