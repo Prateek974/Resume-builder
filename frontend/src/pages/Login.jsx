fc06689c958762a5ca11494e6e557a7e656b8ede
Prateek Sharma <b240754@skit.ac.in>
2026-01-29 22:41:26 +0530

    login page started

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Login = () => {
+    return (
+        <div class="flex h-screen items-center justify-center">
+            <div class="bg-white p-6 shadow-lg">Your Resume Form</div>
+        </div>
+
+
+    );
+};
+export default Login;
